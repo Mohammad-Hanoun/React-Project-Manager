e1e62b79e640b2a5dcf39d9978917a99ac0a9c43
Mohammad Hanoun <hmo115935@gmail.com>
2025-02-18 16:46:01 +0300

    nothing special

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,9 +1,9 @@
-import noProjectImage from "../assets/no-project.svg";
+import noProjectImage from "../assets/no-project.png";
 
 export default function NoProjectSelected() {
   return (
     <div className="mt-24 text-center w-2/3">
-      <img />
+      <img src={noProjectImage} alt="An empty task list"/>
       <h2>No Project Selected</h2>
       <p>Select a project or get started with a new one</p>
       <p>
