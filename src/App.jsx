69c96f6818e1b53aafb4991e552737b2bb374713
Mohammad Hanoun <hmo115935@gmail.com>
2025-02-19 04:41:13 +0300

    projects now appear on the side-bar when saved !

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleSaveProject(projectData) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleSaveProject} />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-     <ProjectsSidebar />
-     <NoProjectSelected />
+      <ProjectsSidebar
+        onStartAddProject={handleAddProject}
+        projects={projectState.projects}
+      />
+      {content}
     </main>
   );
 }
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,6 +1,6 @@
 import noProjectImage from "../assets/no-projects.png";
 import Button from "./Button.jsx";
-export default function NoProjectSelected() {
+export default function NoProjectSelected({onStartAddProject}) {
   return (
     <div className="mt-24 text-center w-2/3">
       <img
@@ -13,7 +13,7 @@ export default function NoProjectSelected() {
       </h2>
       <p className="text-stone-400 mb-4">Select a project or get started with a new one</p>
       <p className="mt-8">
-        <Button>+ New Project</Button>
+        <Button onClick={onStartAddProject}>+ New Project</Button>
       </p>
     </div>
   );
